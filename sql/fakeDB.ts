830d07a94412c3591cd41cf6f166e58df80afb6d
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2022-10-13 12:39:35 -0300

    Moved fake data from route

---
+++ sql/fakeDB.ts
@@ -0,0 +1,263 @@
+export default {
+
+    personagens: new Array(10).fill({
+        id: 1,
+        name: "Cleide Belinha",
+        icon: "cleide_belinha.jpeg",
+        level: 1,
+        exp: 5,
+        tormento: {
+            main: "Vício em",
+            sub: "Adrenalina"
+        },
+        atributos: {
+            fisico: 1,
+            agilidade: 1,
+            inteligencia: 1,
+            coragem: 1
+        },
+        hp: 11,
+        defesa: 5,
+        iniciativa: 2,
+        acoes: 2,
+        antecedentesPontos: 5,
+        antecedentes: {
+            combate: 1,
+            labuta: 1,
+            negocio: 1,
+            montaria: 1,
+            tradicao: 1,
+            exploracao: 1,
+            roubo: 1,
+            medicina: 1,
+        },
+        habilidades: [
+            {
+                ativa: true,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: true,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+            {
+                ativa: false,
+                nome: "Carry on my wayward son",
+                descricao: "Inspirações e bônus para outros personagens"
+            },
+        ],
+        recompensa: 0,
+        dinheiro: 150,
+        equipamentos: [
+            {
+                nome: "Rifle de precisão",
+                dano: "2d6+3 para acertar de perto"
+            },
+            {
+                nome: "Rifle de precisão",
+                dano: "2d6+3 para acertar de perto"
+            },
+            {
+                nome: "Rifle de precisão",
+                dano: "2d6+3 para acertar de perto"
+            },
+            {
+                nome: "Rifle de precisão",
+                dano: "2d6+3 para acertar de perto"
+            },
+            {
+                nome: "Rifle de precisão",
+                dano: "2d6+3 para acertar de perto"
+            },
+            {
+                nome: "Rifle de precisão",
+                dano: "2d6+3 para acertar de perto"
+            },
+            {
+                nome: "Rifle de precisão",
+                dano: "2d6+3 para acertar de perto"
+            },
+            {
+                nome: "Rifle de precisão",
+                dano: "2d6+3 para acertar de perto"
+            },
+            {
+                nome: "Rifle de precisão",
+                dano: "2d6+3 para acertar de perto"
+            },
+            {
+                nome: "Rifle de precisão",
+                dano: "2d6+3 para acertar de perto"
+            }
+        ],
+        cavalo: {
+            nome: "Não Binare",
+            hp: 20,
+            dano: 6,
+            def: 5,
+            atributos: {
+                fidelidade: 1,
+                potencia: 1,
+                vigor: 1
+            },
+            itens: [
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                },
+                {
+                    nome: "Rifle de precisão",
+                }
+            ]
+        }
+    }),
+
+    getPersonagens() {
+        return this.personagens
+    }
+}
